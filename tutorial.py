a3010fc8672c2e44aa338d36130b3abd18f9797a
Johannes <johannes.ai@proton.me>
2023-10-09 19:55:04 -0600

    Implemented zoom and fixed errors.
    Also named the planets instead of the numbers.

--- tutorial.py
+++ tutorial.py
@@ -1,8 +1,9 @@
 import pygame
 import math
+
 pygame.init()
 
-WIDTH, HEIGHT =  800, 800
+WIDTH, HEIGHT = 500, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
@@ -15,114 +16,104 @@ DARK_GREY = (80, 78, 81)
 FONT = pygame.font.SysFont("comicsans", 16)
 
 class Planet:
-	AU = 149.6e6 * 1000
-	G = 6.67428e-11
-	SCALE = 250 / AU  # 1AU = 100 pixels
-	TIMESTEP = 3600*24 # 1 day
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250 / AU
+    TIMESTEP = 3600 * 12  # Adjusted for slower simulation (half a day)
 
-	def __init__(self, x, y, radius, color, mass):
-		self.x = x
-		self.y = y
-		self.radius = radius
-		self.color = color
-		self.mass = mass
+    def __init__(self, name, x, y, radius, color, mass):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+        self.x_vel = 0
+        self.y_vel = 0
 
-		self.orbit = []
-		self.sun = False
-		self.distance_to_sun = 0
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        if len(self.orbit) > 2:
+            updated_points = [(point[0] * self.SCALE + WIDTH / 2, point[1] * self.SCALE + HEIGHT / 2) for point in self.orbit]
+            pygame.draw.lines(win, self.color, False, updated_points, 2)
+        pygame.draw.circle(win, self.color, (int(x), int(y)), self.radius)
+        planet_name_text = FONT.render(self.name, 1, WHITE)
+        win.blit(planet_name_text, (int(x) + self.radius + 5, int(y) - planet_name_text.get_height()//2))
+        if not self.sun:
+            distance_text = FONT.render(f"{round(self.distance_to_sun/1000, 1)}km", 1, WHITE)
+            win.blit(distance_text, (int(x) - distance_text.get_width()/2, int(y) - distance_text.get_height()/2 - planet_name_text.get_height()))
 
-		self.x_vel = 0
-		self.y_vel = 0
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
 
-	def draw(self, win):
-		x = self.x * self.SCALE + WIDTH / 2
-		y = self.y * self.SCALE + HEIGHT / 2
+        if other.sun:
+            self.distance_to_sun = distance
 
-		if len(self.orbit) > 2:
-			updated_points = []
-			for point in self.orbit:
-				x, y = point
-				x = x * self.SCALE + WIDTH / 2
-				y = y * self.SCALE + HEIGHT / 2
-				updated_points.append((x, y))
+        force = self.G * self.mass * other.mass / distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
 
-			pygame.draw.lines(win, self.color, False, updated_points, 2)
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
 
-		pygame.draw.circle(win, self.color, (x, y), self.radius)
-		
-		if not self.sun:
-			distance_text = FONT.render(f"{round(self.distance_to_sun/1000, 1)}km", 1, WHITE)
-			win.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2))
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
 
-	def attraction(self, other):
-		other_x, other_y = other.x, other.y
-		distance_x = other_x - self.x
-		distance_y = other_y - self.y
-		distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
 
-		if other.sun:
-			self.distance_to_sun = distance
-
-		force = self.G * self.mass * other.mass / distance**2
-		theta = math.atan2(distance_y, distance_x)
-		force_x = math.cos(theta) * force
-		force_y = math.sin(theta) * force
-		return force_x, force_y
-
-	def update_position(self, planets):
-		total_fx = total_fy = 0
-		for planet in planets:
-			if self == planet:
-				continue
-
-			fx, fy = self.attraction(planet)
-			total_fx += fx
-			total_fy += fy
-
-		self.x_vel += total_fx / self.mass * self.TIMESTEP
-		self.y_vel += total_fy / self.mass * self.TIMESTEP
-
-		self.x += self.x_vel * self.TIMESTEP
-		self.y += self.y_vel * self.TIMESTEP
-		self.orbit.append((self.x, self.y))
-
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
 
 def main():
-	run = True
-	clock = pygame.time.Clock()
+    run = True
+    clock = pygame.time.Clock()
 
-	sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
-	sun.sun = True
+    sun = Planet("Sun", 0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+    earth = Planet("Earth", -1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
+    mars = Planet("Mars", -1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
+    mercury = Planet("Mercury", 0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    mercury.y_vel = -47.4 * 1000
+    venus = Planet("Venus", 0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
+    planets = [sun, earth, mars, mercury, venus]
 
-	earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
-	earth.y_vel = 29.783 * 1000 
+    while run:
+        clock.tick(60)
+        WIN.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            # Zoom in/out with mouse wheel
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:  # Scroll up
+                    Planet.SCALE *= 1.1
+                if event.button == 5:  # Scroll down
+                    Planet.SCALE /= 1.1
 
-	mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
-	mars.y_vel = 24.077 * 1000
+        for planet in planets:
+            planet.update_position(planets)
+            planet.draw(WIN)
 
-	mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
-	mercury.y_vel = -47.4 * 1000
+        pygame.display.update()
 
-	venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
-	venus.y_vel = -35.02 * 1000
-
-	planets = [sun, earth, mars, mercury, venus]
-
-	while run:
-		clock.tick(60)
-		WIN.fill((0, 0, 0))
-
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				run = False
-
-		for planet in planets:
-			planet.update_position(planets)
-			planet.draw(WIN)
-
-		pygame.display.update()
-
-	pygame.quit()
-
+    pygame.quit()
 
 main()
